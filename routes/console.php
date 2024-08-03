eeeaa23b2584e65f7c8e69e215ba1efe7c29116f
Janic Scheinhardt <github@jsnh.dev>
2024-08-03 19:34:28 +0200

    console route youtube

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,5 @@ Schedule::command('app:x')->hourly();
 
 Schedule::command('app:twitch')->hourly()->runInBackground();
 Schedule::command('app:twitch_stream')->everyMinute()->runInBackground();
+
+Schedule::command('app:youtube')->hourly();
